2692801f1423beaacb5b6f8df3c27fa3b3b17307
PratyushJha19 <jhapratyush2006@gmail.com>
2023-07-24 16:34:21 +0530

    Configuring Django Image for Uploads
    also installed PILLOW

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'static/media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
