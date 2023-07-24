a87bce5a7a135120f88f9fcab6dbf197baaf4186
PratyushJha19 <jhapratyush2006@gmail.com>
2023-07-24 15:55:54 +0530

    Initial commit

---
+++ ecommerce/store/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, db_index=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    brand = models.CharField(max_length=250, default='Un-Branded')
+    description = models.TextField(blank=True)
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=3)
+    # image = models.ImageField(upload_to='images/')
+
+    class Meta:
+        verbose_name_plural = 'Products'
+    
+    def __str__(self):
+        return self.title
