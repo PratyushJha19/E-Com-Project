5cfdbbf48825cb846ff565e0c4d3319fb0e505cd
PratyushJha19 <jhapratyush2006@gmail.com>
2023-07-24 16:59:49 +0530

    Admin Configuration

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    # 'Slug' inherits its value from 'name'
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
