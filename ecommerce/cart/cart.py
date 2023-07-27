39debb614c0963c4b2dbf3390ccd610d94b3e909
PratyushJha19 <jhapratyush2006@gmail.com>
2023-07-27 17:15:03 +0530

    Add Product to Cart Functionality - 1
    Using Asynchronous AJAX JS

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -10,3 +10,13 @@ class Cart():
             cart = self.session['session_key'] = {}
 
         self.cart = cart
+
+    def add(self, product, product_qty):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            self.cart[product_id]['qty'] = product_qty
+        
+        else:
+            self.cart[product_id] = {'price':str(product.price), 'qty':product_qty}
+        
+        self.session.modified = True
--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from .cart import Cart
+from store.models import Product
+from django.shortcuts import get_object_or_404
 
 def cart_summary(request):
     return render(request, 'cart/cart_summary.html')
 
 def cart_add(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'POST':
+        product_id = int(request.POST.get('product_id'))
+        product_quantity = int(request.POST.get('product_quantity'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product, product_qty=product_quantity)
 
 def cart_delete(request):
     pass
