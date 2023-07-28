a5dce2ab00b481c6d38575185309ba1cdbb3442b
PratyushJha19 <jhapratyush2006@gmail.com>
2023-07-28 12:22:37 +0530

    Add to Cart Functionality - 2

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -20,3 +20,6 @@ class Cart():
             self.cart[product_id] = {'price':str(product.price), 'qty':product_qty}
         
         self.session.modified = True
+
+    def __len__(self):
+        return sum(item['qty'] for item in self.cart.values())
--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -2,17 +2,23 @@ from django.shortcuts import render
 from .cart import Cart
 from store.models import Product
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 
 def cart_summary(request):
     return render(request, 'cart/cart_summary.html')
 
 def cart_add(request):
     cart = Cart(request)
-    if request.POST.get('action') == 'POST':
+    if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('product_id'))
         product_quantity = int(request.POST.get('product_quantity'))
         product = get_object_or_404(Product, id=product_id)
         cart.add(product=product, product_qty=product_quantity)
+
+        cart_quantity = cart.__len__()
+
+        response = JsonResponse({'qty': cart_quantity})
+        return response
 
 def cart_delete(request):
     pass
