a3f669d339dd6c0e38d6a1009feb25ed96222259
PratyushJha19 <jhapratyush2006@gmail.com>
2023-08-07 13:46:29 +0530

    Total Price Functionality in Cart Page

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -40,3 +40,6 @@ class Cart():
             item['total'] = item['price'] * item['qty']
             yield item
 
+    def get_total(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.cart.values())
+
